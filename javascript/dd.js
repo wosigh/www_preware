7bdc2eb709d75ef2912de0f4adfd85618b3b169e
Templarian <admin@templarian.com>
2009-12-09 00:10:25 -0500

    small define.

--- javascript/dd.js
+++ javascript/dd.js
@@ -750,6 +750,7 @@ dd.xml = function(o)
                 var temp = new ActiveXObject("Microsoft.XMLDOM");
                 temp.async="false";
                 temp.loadXML(text);
+                this.fn.xml.doc = {};
                 this.fn.xml.doc.childNodes = [];
                 this.fn.xml.doc.childNodes[0] = temp.childNodes[1];
             }
