c0100129e05325b9160813a083290afbfe318896
Templarian <admin@templarian.com>
2009-12-10 17:50:29 -0500

    opacity again

--- javascript/dd.js
+++ javascript/dd.js
@@ -416,7 +416,9 @@ dd.fn = {
         else
         {
             alert((i * 100));
-            this.e.setAttribute("style", "filter:progid:DXImageTransform.Microsoft.Alpha(opacity=" + (i * 100) + "),alpha(opacity = " + (i * 100) + ")");
+            if("filters" in this.e && typeof this.e.filters.alpha.opacity == "number")
+                this.e.filters.item("DXImageTransform.Microsoft.Alpha").opacity = (i * 100);
+            //this.e.setAttribute("style", "filter:progid:DXImageTransform.Microsoft.Alpha(opacity=" + (i * 100) + "),alpha(opacity = " + (i * 100) + ")");
             this.e.style.opacity = i;
             return i;
         }
