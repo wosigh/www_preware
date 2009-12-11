8f8a7b4dd8e113d3898955798b58be1064794bac
Templarian <admin@templarian.com>
2009-12-10 21:15:38 -0500

    opacity removed alert

--- javascript/dd.js
+++ javascript/dd.js
@@ -415,9 +415,7 @@ dd.fn = {
         }
         else
         {
-            alert((i * 100));
-            if("filters" in this.e)
-                this.e.filters = "progid:DXImageTransform.Microsoft.Alpha(opacity=" + (i * 100) + "),alpha(opacity = " + (i * 100) + ")";
+            this.e.style.filter = "progid:DXImageTransform.Microsoft.Alpha(opacity=" + (i * 100) + "),alpha(opacity = " + (i * 100) + ")";
             this.e.style.opacity = i;
             return i;
         }
